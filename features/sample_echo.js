dd546374c1960d1c346e9d979fe75ab62fecea56
Connie Lok <conniehlok@gmail.com>
2020-08-03 13:51:22 -0400

    feat: get cuisines

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -3,14 +3,37 @@
  * Licensed under the MIT License.
  */
 
+const axios = require('axios');
+
 module.exports = function(controller) {
+  controller.hears(
+    ['hi', 'hello', 'howdy', 'hey', 'aloha', 'hola', 'bonjour', 'oi'],
+    'message,direct_message',
+    async (bot, message) => {
+      await bot.reply(
+        message,
+        'Hi there! What kind of cuisine are you in the mood for? You can also type "help" for some options.'
+      );
+    }
+  );
 
-    controller.hears('sample','message,direct_message', async(bot, message) => {
-        await bot.reply(message, 'I heard a sample message.');
-    });
+  //   controller.on('message', async (bot, message) => {
+  //     await bot.reply(message, {
+  //       text: 'What kind of cuisine are you in the mood for?',
+  //       quick_replies: [
+  //         {
+  //           title: 'Categories',
+  //           payload: 'categories',
+  //         },
+  //         {
+  //           title: 'Help',
+  //           payload: 'help',
+  //         },
+  //       ],
+  //     });
+  //   });
 
-    controller.on('message,direct_message', async(bot, message) => {
-        await bot.reply(message, `Echo: ${ message.text }`);
-    });
-
-}
+  //   controller.on('message,direct_message', async (bot, message) => {
+  //     await bot.reply(message, `Echo: ${message.text}`);
+  //   });
+};
--- features/sample_hears.js
+++ features/sample_hears.js
@@ -2,21 +2,59 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
+
+const axios = require('axios');
+
 module.exports = function(controller) {
+  // use a function to match a condition in the message
+  controller.hears(
+    async (message) => message.text && message.text.toLowerCase() === 'help',
+    ['message'],
+    async (bot, message) => {
+      const { data } = await axios({
+        method: 'GET',
+        url: 'https://developers.zomato.com/api/v2.1/cuisines?city_id=280',
+        headers: {
+          'user-key': process.env.ZOMATO_KEY,
+          'content-type': 'application/json',
+        },
+      });
+      let cuisines = data.cuisines;
+      let cuisineOptions = [];
+      for (let i = 0; i < 5; i++) {
+        let randIdx = Math.floor(Math.random() * cuisines.length);
+        cuisineOptions.push(cuisines[randIdx].cuisine);
+      }
 
-    // use a function to match a condition in the message
-    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'foo', ['message'], async (bot, message) => {
-        await bot.reply(message, 'I heard "foo" via a function test');
-    });
+      await bot.reply(message, {
+        text: `Here's some ideas! Listen to your stomach 🤤`,
+        quick_replies: cuisineOptions.map((cuisine) => {
+          return {
+            title: cuisine.cuisine_name,
+            payload: cuisine.cuisine_name,
+          };
+        }),
+      });
+    }
+  );
 
-    // use a regular expression to match the text of the message
-    controller.hears(new RegExp(/^\d+$/), ['message','direct_message'], async function(bot, message) {
-        await bot.reply(message,{ text: 'I heard a number using a regular expression.' });
-    });
+  // use a regular expression to match the text of the message
+  controller.hears(
+    new RegExp(/^\d+$/),
+    ['message', 'direct_message'],
+    async function(bot, message) {
+      await bot.reply(message, {
+        text: 'I heard a number using a regular expression.',
+      });
+    }
+  );
 
-    // match any one of set of mixed patterns like a string, a regular expression
-    controller.hears(['allcaps', new RegExp(/^[A-Z\s]+$/)], ['message','direct_message'], async function(bot, message) {
-        await bot.reply(message,{ text: 'I HEARD ALL CAPS!' });
-    });
-
-}
+  // match any one of set of mixed patterns like a string, a regular expression
+  controller.hears(
+    ['allcaps', new RegExp(/^[A-Z\s]+$/)],
+    ['message', 'direct_message'],
+    async function(bot, message) {
+      await bot.reply(message, { text: 'I HEARD ALL CAPS!' });
+    }
+  );
+};
